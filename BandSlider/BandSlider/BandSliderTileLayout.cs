f24c6c9ae86e252c383175ef3fe61ae335ff2aa5
proemmer <benjamin.proemmer@insite-gmbh.de>
2016-04-30 12:16:50 +0200

    Initial Commit

---
+++ BandSlider/BandSlider/BandSliderTileLayout.cs
@@ -0,0 +1,139 @@
+using Microsoft.Band;
+using Microsoft.Band.Tiles;
+using Microsoft.Band.Tiles.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace BandSlider
+{
+	internal class BandSliderTileLayout
+	{
+		private readonly PageLayout pageLayout;
+		private readonly PageLayoutData pageLayoutData;
+		
+		
+		
+		public BandSliderTileLayout()
+		{
+			LoadIconMethod = LoadIcon;
+			AdjustUriMethod = (uri) => uri;
+			
+			pageLayout = new PageLayout();
+			
+			PageElementData[] pageElementDataArray = new PageElementData[0];
+			
+			pageLayoutData = new PageLayoutData(pageElementDataArray);
+		}
+		
+		public PageLayout Layout
+		{
+			get
+			{
+				return pageLayout;
+			}
+		}
+		
+		public PageLayoutData Data
+		{
+			get
+			{
+				return pageLayoutData;
+			}
+		}
+		
+		public Func<string, Task<BandIcon>> LoadIconMethod
+		{
+			get;
+			set;
+		}
+		
+		public Func<string, string> AdjustUriMethod
+		{
+			get;
+			set;
+		}
+		
+		private static async Task<BandIcon> LoadIcon(string uri)
+		{
+			StorageFile imageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri(uri));
+			
+			using (IRandomAccessStream fileStream = await imageFile.OpenAsync(FileAccessMode.Read))
+			{
+				WriteableBitmap bitmap = new WriteableBitmap(1, 1);
+				await bitmap.SetSourceAsync(fileStream);
+				return bitmap.ToBandIcon();
+			}
+		}
+		
+		public async Task LoadIconsAsync(BandTile tile)
+		{
+			await Task.Run(() => { }); // Dealing with CS1998
+		}
+		
+		public static BandTheme GetBandTheme()
+		{
+			var theme = new BandTheme();
+			theme.Base = new BandColor(51, 102, 204);
+			theme.HighContrast = new BandColor(58, 120, 221);
+			theme.Highlight = new BandColor(58, 120, 221);
+			theme.Lowlight = new BandColor(49, 101, 186);
+			theme.Muted = new BandColor(43, 90, 165);
+			theme.SecondaryText = new BandColor(137, 151, 171);
+			return theme;
+		}
+		
+		public static BandTheme GetTileTheme()
+		{
+			var theme = new BandTheme();
+			theme.Base = new BandColor(51, 102, 204);
+			theme.HighContrast = new BandColor(58, 120, 221);
+			theme.Highlight = new BandColor(58, 120, 221);
+			theme.Lowlight = new BandColor(49, 101, 186);
+			theme.Muted = new BandColor(43, 90, 165);
+			theme.SecondaryText = new BandColor(137, 151, 171);
+			return theme;
+		}
+		
+		public class PageLayoutData
+		{
+			private readonly PageElementData[] array;
+			
+			public PageLayoutData(PageElementData[] pageElementDataArray)
+			{
+				array = pageElementDataArray;
+			}
+			
+			public int Count
+			{
+				get
+				{
+					return array.Length;
+				}
+			}
+			
+			public T Get<T>(int i) where T : PageElementData
+			{
+				return (T)array[i];
+			}
+			
+			public T ById<T>(short id) where T:PageElementData
+			{
+				return (T)array.FirstOrDefault(elm => elm.ElementId == id);
+			}
+			
+			public PageElementData[] All
+			{
+				get
+				{
+					return array;
+				}
+			}
+		}
+		
+	}
+}
