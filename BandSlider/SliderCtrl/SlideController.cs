f296d718c3051bd005f0667ebd5635d9ddc9f617
proemmer <benjamin.proemmer@insite-gmbh.de>
2016-06-07 21:42:05 +0200

    Adding SliderControll :  Addin for powerpoint which opened a webinterface to control the presentation

---
+++ BandSlider/SliderCtrl/SlideController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace SliderCtrl
+{
+    public class SlideController : ApiController
+    {
+
+        public SlideController()
+        {
+
+        }
+
+        [HttpGet]
+        public int Page()
+        {
+            return Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View.Slide.SlideIndex;
+        }
+
+        [HttpPost]
+        public void Start()
+        {
+            Globals.ThisAddIn.Application.ActivePresentation.SlideShowSettings.Run();
+        }
+
+        [HttpPost]
+        public void Stop()
+        {
+            Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View.Exit();
+        }
+
+        [HttpPost]
+        public void Next()
+        {
+            Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View.Next();
+        }
+
+        [HttpPost]
+        public void Prev()
+        {
+            Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View.Previous();
+        }
+    }
+}
---
+++ BandSlider/SliderCtrl/ThisAddIn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+
+namespace SliderCtrl
+{
+    public partial class ThisAddIn
+    {
+        private HttpSelfHostServer _server;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:5000");
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional });
+
+            _server = new HttpSelfHostServer(config);
+            _server.OpenAsync().Wait();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            if (_server != null)
+                _server.Dispose();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
